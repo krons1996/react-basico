68f99deeebdc86e5d6c667700899d3173a503b8d
krons1996 <krons1996@gmail.com>
2022-03-22 22:38:17 -0300

    Alterando itens do components Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,8 +7,6 @@ const Menu = () => {
             <li><a href="">Página 1</a></li>
             <li><a href="">Página 2</a></li>
             <li><a href="">Página 3</a></li>
-            <li><a href="">Página 2</a></li>
-            <li><a href="">Página 3</a></li>
             
         </ul>
     </div>
